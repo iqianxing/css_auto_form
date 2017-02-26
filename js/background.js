463285cf93f38138e70b47ba5022b0268c8d3ae7
iqianxing <qianxingqianli@qq.com>
2017-02-26 22:50:27 +0800

    first commit

---
+++ js/background.js
@@ -0,0 +1,43 @@
+﻿(function (window, $) {
+	$(document.body).append('<form class="form form-horizontal">' +
+		'<div css="control-group" id="AutoForm">' +
+		'<label class="control-label">任务：</label>' +
+		'<div class="controls">' +
+		'<textarea type="text" id="taskList" rows=5></textarea>' +
+		'<button type="button" class="btn btn-primary" id="fillForm">填单</button>' +
+		'</div>' +
+		'</div>' +
+		'</form>');
+
+
+	$("#fillForm").on("click", action);
+
+	function action() {
+		var taskList = $("#taskList").val().split("\n");
+		taskList.forEach(function (v, i) {
+			if (!v || !v.trim()) {
+				return;
+			}
+
+			var kv = v.split("||");
+			setValue(kv[0], kv[1]);
+		})
+
+	}
+
+	function setValue(selector, value) {
+		console.log("css:%s\nvalue:%s", selector, value);
+		$(selector).val(value);
+	}
+
+	function doAction(selector, action) {
+		console.log("css:%s\naction:%s", selector, action);
+		$(selector)[action]();
+	}
+
+
+	window.AV = {
+		setValue: setValue,
+		doAction: doAction,
+	}
+})(window, jQuery);
